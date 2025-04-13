0a771e50828eb5af3636e38bf175df216fe4e300
Philipp Schmitt <philipp@schmitt.co>
2025-04-13 13:08:41 +0200

    syntax

--- lib/default.nix
+++ lib/default.nix
@@ -866,9 +866,17 @@ let
               umount -Rv "${rootMountPoint}" || :
 
               # shellcheck disable=SC2043,2041
-              for dev in ${toString (lib.catAttrs "device" (lib.filterAttrs (name: disk: !(disk.noDestroy or false)) devices.disk))}; do
+              for dev in ${
+                toString (
+                  lib.catAttrs "device" (
+                    lib.attrValues (lib.filterAttrs (name: disk: !(disk.noDestroy or false)) devices.disk)
+                  )
+                )
+              };
+              do
                 $BASH ${../disk-deactivate}/disk-deactivate "$dev"
               done
+
             '';
           };
           _destroy = lib.mkOption {
@@ -879,7 +887,9 @@ let
             '';
             default =
               let
-                selectedDisks = lib.escapeShellArgs (lib.catAttrs "device" (lib.filterAttrs (name: disk: !(disk.noDestroy or false)) devices.disk));
+                selectedDisks = lib.escapeShellArgs (
+                  lib.catAttrs "device" (lib.filterAttrs (name: disk: !(disk.noDestroy or false)) devices.disk)
+                );
               in
               ''
                 if [ "$1" != "--yes-wipe-all-disks" ]; then
@@ -904,7 +914,11 @@ let
                 umount -Rv "${rootMountPoint}" || :
 
                 # shellcheck disable=SC2043,2041
-                for dev in ${toString (lib.catAttrs "device" (lib.filterAttrs (name: disk: !(disk.noDestroy or false)) devices.disk))}; do
+                for dev in ${
+                  toString (
+                    lib.catAttrs "device" (lib.filterAttrs (name: disk: !(disk.noDestroy or false)) devices.disk)
+                  )
+                }; do
                   $BASH ${../disk-deactivate}/disk-deactivate "$dev"
                 done
               '';
