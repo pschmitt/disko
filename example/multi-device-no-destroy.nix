b46a9324338c1caf3a69a9a0329372d7c133cb70
Philipp Schmitt <philipp@schmitt.co>
2025-04-18 20:33:30 +0200

    WIP tests: fix disko config and copy it into the vm

--- example/multi-device-no-destroy.nix
+++ example/multi-device-no-destroy.nix
@@ -1,37 +1,45 @@
 {
-  disko.devices = {
-    disk = {
-      root = {
-        device = "/dev/vda";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
+  disko.devices.disk = {
+    root = {
+      device = "/dev/vda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            type = "EF00";
+            size = "500M";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
             };
           };
         };
       };
-      data = {
-        device = "/dev/vdb";
-        type = "disk";
-        destroy = false;
-        content = {
-          type = "gpt";
-          partitions = {
-            data = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/mnt/data";
-              };
+    };
+    data = {
+      device = "/dev/vdb";
+      type = "disk";
+      destroy = false;
+      content = {
+        type = "gpt";
+        partitions = {
+          data = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/mnt/data";
             };
           };
         };
--- tests/no-destroy.nix
+++ tests/no-destroy.nix
@@ -3,25 +3,22 @@
   diskoLib ? pkgs.callPackage ../lib { },
 }:
 
-diskoLib.testLib.makeDiskoTest (
-  let
-    configFile = pkgs.writeTextFile {
-      name = "multi-device-no-destroy-config";
-      text = builtins.readFile ../example/multi-device-no-destroy.nix;
-    };
-  in
-  {
-    inherit pkgs;
-    name = "multi-device-no-destroy";
-    disko-config = ../example/multi-device-no-destroy.nix;
-    extraTestScript = ''
-      # Write a test file to the data disk after initial provisioning
-      machine.succeed("echo -n disko > /mnt/mnt/data/test.txt");
-      # Reprovision the system using the same Disko config (root disk will be reformatted)
-      machine.succeed("${pkgs.coreutils}/bin/cp ${configFile} /tmp/multi-device-no-destroy.nix");
-      machine.succeed("${pkgs.disko}/bin/disko --mode destroy,format,mount /tmp/multi-device-no-destroy.nix --yes-wipe-all-disks");
-      # Verify the data disk still contains the test file with correct content
-      machine.succeed("grep -Fx disko /mnt/mnt/data/test.txt");
-    '';
-  }
-)
+diskoLib.testLib.makeDiskoTest ({
+  inherit pkgs;
+  name = "multi-device-no-destroy";
+  disko-config = ../example/multi-device-no-destroy.nix;
+  extraTestScript = ''
+    # Write a test file to the data disk after initial provisioning
+    machine.succeed("echo -n disko > /mnt/data/test.txt");
+
+    # Reprovision the system using the same Disko config (root disk should be
+    # reformatted, but not the data disk)
+    machine.succeed("${pkgs.coreutils}/bin/cp -vf '${../example/multi-device-no-destroy.nix}' /tmp/disko-config.nix");
+
+    machine.succeed("cat -A /tmp/disko-config.nix >&2");
+    machine.succeed("${pkgs.disko}/bin/disko --debug --mode destroy,format,mount /tmp/disko-config.nix --yes-wipe-all-disks");
+
+    # Verify the data disk still contains the test file with correct content
+    machine.succeed("grep -Fx disko /mnt/data/test.txt");
+  '';
+})
